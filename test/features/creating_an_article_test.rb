ee96940ed8fe0d0efc43434501d81a680dd7e1fa
Derek Maffett <derekgmaffett@gmail.com>
2014-09-17 21:02:13 -0700

    implement GWT comments for clarity and practice

--- test/features/creating_an_article_test.rb
+++ test/features/creating_an_article_test.rb
@@ -2,10 +2,15 @@ require_relative '../test_helper.rb'
 
 feature "creating an article" do
   scenario "submit form data to create a new article" do
+    # Given: An article does not exist
+
+    # When: A form is submitted for a new article
     visit new_article_path
     fill_in "Title", with: "Test article"
     fill_in "Body", with: "Behaviour driven development is a lot of fun!"
     click_on "Create Article"
+
+    # Then: A new article should show up on the show page
     page.must_have_content "Test article"
     page.must_have_content "Behaviour driven development"
   end
--- test/features/visiting_the_article_index_test.rb
+++ test/features/visiting_the_article_index_test.rb
@@ -2,11 +2,16 @@ require_relative '../test_helper.rb'
 
 feature "visiting the article index" do
   scenario "with existing articles, show list" do
+    # Given: An article exists
     Article.create(
       title: "Becoming a Code Fellow",
       body: "means striving for excellence."
       )
+
+    # When: The index page is visited
     visit articles_path
+
+    # Then: Content from the article should be displayed
     page.must_have_content "Code Fellow"
   end
 end
