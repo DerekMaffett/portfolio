054ce279df69050a1597aa04b8c106d55591a710
Derek Maffett <derekgmaffett@gmail.com>
2014-09-23 16:08:40 -0700

    add image_url to projects for linking with Amazon S3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923013154) do
+ActiveRecord::Schema.define(version: 20140923225501) do
 
   create_table "articles", force: true do |t|
     t.text     "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140923013154) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
 end
